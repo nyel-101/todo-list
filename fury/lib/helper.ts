8ab387a4d7654f2d5f14dadc15ac416637491ad5
nyel-101 <dandevjr@gmail.com>
2025-02-16 09:04:10 +0000

    feat(ui): optimize session store

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8c5b48ce6e64af0b2ba8c7c8d7e66c3 = () => {
-  return '2026-05-05T04:47:21Z';
+const node_967f77192b3c4bf3a8014381deb05b70 = () => {
+  return '2025-02-16T09:04:10Z';
 };
