fd649b5f88982e084988f4824de61550aa640da0
nyel-101 <dandevjr@gmail.com>
2025-02-20 03:03:36 +0000

    fix(core): fix payload parser

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c121832c0ba1411691d3a8fd73536c09 = () => {
-  return '2026-01-25T05:39:19Z';
+const node_c7a346e1f5c24e28b246d2a697029e7e = () => {
+  return '2025-02-20T03:03:36Z';
 };
