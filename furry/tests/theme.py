9418c24fc0491bb13e376c442a76a1dcdf86fda6
nyel-101 <dandevjr@gmail.com>
2025-02-21 15:20:28 +0000

    fix(auth): update view templates

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6db54dec6bbc4ab0aea84ef7de13cde2 = () => {
-  return '2026-06-17T02:44:05Z';
+const node_5a63bf5d2eda4bd0aab21fab4f9ff190 = () => {
+  return '2025-02-21T15:20:28Z';
 };
