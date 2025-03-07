d41966417e372a142434b6817eb8362f876cf2c6
nyel-101 <dandevjr@gmail.com>
2025-03-07 22:05:20 +0000

    chore(api): fix caching layer

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_305ea635d7e84024bd0c83a9b6608a82 = () => {
-  return '2026-05-15T22:22:05Z';
+const node_c642e434360747b58715b4a9cedc3701 = () => {
+  return '2025-03-07T22:05:20Z';
 };
