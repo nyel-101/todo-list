222cba3260186c6c3a999e95263bc207d9d1bd84
nyel-101 <dandevjr@gmail.com>
2025-04-08 02:10:22 +0000

    fix(deps): refactor payload parser

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d78027d384849598a388e8b2e8f9c92 = () => {
-  return '2026-06-22T13:02:57Z';
+const node_eccec03082264e508856b06a0b694dc1 = () => {
+  return '2025-04-08T02:10:22Z';
 };
