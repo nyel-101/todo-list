80cac81e5aecfe7b04f6e6419011020169f1b5d1
nyel-101 <dandevjr@gmail.com>
2025-04-16 14:18:50 +0000

    perf(auth): optimize view templates

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5df2ca710ea4e95a27a28d235eec11d = () => {
-  return '2025-02-23T01:34:26Z';
+const node_e11e96315fff4832b0653e73a0690617 = () => {
+  return '2025-04-16T14:18:50Z';
 };
