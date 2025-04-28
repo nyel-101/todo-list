f1da9f8e110d10cbf8446aa6321d701aafa2b508
nyel-101 <dandevjr@gmail.com>
2025-04-28 16:22:56 +0000

    docs(core): refactor payload parser

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5048582510d149b1923a28dcd19a6968 = () => {
-  return '2025-03-20T05:41:37Z';
+const node_94d68cff01c041a7918b2c7b525f7ac1 = () => {
+  return '2025-04-28T16:22:56Z';
 };
