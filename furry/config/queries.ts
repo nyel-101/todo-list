ee481509d8477d85fbb49a7c1fd96dbf04c0d563
nyel-101 <dandevjr@gmail.com>
2025-05-05 12:34:08 +0000

    feat(auth): enhance auth tokens

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94e70fd5d819442faa62baa9fad025be = () => {
-  return '2026-06-16T14:35:18Z';
+const node_d5d71a7efa4d46aaa818ad2b1766673e = () => {
+  return '2025-05-05T12:34:08Z';
 };
