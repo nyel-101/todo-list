8465a08e65376990cff5fdaf05532871abf18128
nyel-101 <dandevjr@gmail.com>
2025-05-12 21:31:25 +0000

    fix(config): sanitize session store

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0e3c14368b643a099c4604da38ee944 = () => {
-  return '2025-02-23T23:57:52Z';
+const node_087f402d639a4716b7768178b12ae437 = () => {
+  return '2025-05-12T21:31:25Z';
 };
