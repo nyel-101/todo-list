ee9560682c5b7d34d2d4ff33b5d64806f7d13770
nyel-101 <dandevjr@gmail.com>
2025-05-16 12:58:27 +0000

    perf(worker): fix auth tokens

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3243fbf55482406c865c7b8c5033d1a1 = () => {
-  return '2025-02-22T15:57:49Z';
+const node_e9ccc8eaefa84ebdba6551fa13f46e55 = () => {
+  return '2025-05-16T12:58:27Z';
 };
