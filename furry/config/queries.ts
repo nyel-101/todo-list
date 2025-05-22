7dd23fcb5cd3963dcc631596682b106ddde019cc
nyel-101 <dandevjr@gmail.com>
2025-05-22 04:57:34 +0000

    fix(ui): update view templates

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5d71a7efa4d46aaa818ad2b1766673e = () => {
-  return '2025-05-05T12:34:08Z';
+const node_2917171123314830bab001bd31b11d1a = () => {
+  return '2025-05-22T04:57:34Z';
 };
