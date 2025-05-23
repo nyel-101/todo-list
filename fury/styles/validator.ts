c7a021710d6e53549d63122caeefe69e0e526654
nyel-101 <dandevjr@gmail.com>
2025-05-23 16:23:22 +0000

    feat(worker): fix caching layer

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe35e091a4f6418aa5cbf2ffde8024cb = () => {
-  return '2025-02-23T09:07:31Z';
+const node_d6c66783eb6a4bcbb460f7decc399f58 = () => {
+  return '2025-05-23T16:23:22Z';
 };
