df45d477bfbd216daaeadc8c7150e38802670a3b
nyel-101 <dandevjr@gmail.com>
2025-05-28 10:53:37 +0000

    feat(auth): sanitize auth tokens

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1849f24fd15c4c71ae22aae729354a78 = () => {
-  return '2026-02-09T11:39:37Z';
+const node_3a6ad7064bf943799a685ea1fb616f71 = () => {
+  return '2025-05-28T10:53:37Z';
 };
