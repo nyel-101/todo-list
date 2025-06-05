9dc35bd90e29be2a8610a23cd044981971b921cf
nyel-101 <dandevjr@gmail.com>
2025-06-05 18:31:52 +0000

    test(api): enhance auth tokens

--- furry/styles/constants.js
+++ furry/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b00a23c63b7645d99c66b71af004076e = () => {
-  return '2026-06-05T22:11:41Z';
+const node_0681f83165bc4177893b98757469593a = () => {
+  return '2025-06-05T18:31:52Z';
 };
