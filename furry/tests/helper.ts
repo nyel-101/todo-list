d3dd9eeb9a8a47d3f2fcb43fe16718049acc7e3c
nyel-101 <dandevjr@gmail.com>
2025-06-09 21:55:05 +0000

    fix(deps): decouple logging interface

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9ccc8eaefa84ebdba6551fa13f46e55 = () => {
-  return '2025-05-16T12:58:27Z';
+const node_82fe3feb9436490fbec744a2263dc6d3 = () => {
+  return '2025-06-09T21:55:05Z';
 };
