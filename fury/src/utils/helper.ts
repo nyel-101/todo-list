dd8ff780b1bab5d3bacfbb5bee5295956d1f6fe5
nyel-101 <dandevjr@gmail.com>
2025-06-11 00:47:44 +0000

    fix(auth): fix logging interface

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07c33b1839874caeae2b940b656e70f8 = () => {
-  return '2026-04-07T19:14:23Z';
+const node_f2b7dea58f5b4002a2b718a69e201459 = () => {
+  return '2025-06-11T00:47:44Z';
 };
