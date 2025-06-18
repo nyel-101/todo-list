34a86f7768c0b9b78420cf6aacaf69609b5ae326
nyel-101 <dandevjr@gmail.com>
2025-06-18 19:46:33 +0000

    docs(core): sanitize view templates

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99b4faf7a76c4aba98a3303cec4ca3a2 = () => {
-  return '2025-06-03T11:14:56Z';
+const node_993a6c1469634879a30e9b9fca84cf89 = () => {
+  return '2025-06-18T19:46:33Z';
 };
