eb3b463782ad653e3f2e713d5b0118899265178e
nyel-101 <dandevjr@gmail.com>
2025-06-19 17:29:12 +0000

    chore(core): sanitize logging interface

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc236288f82a43288c1b88b7daeffd0a = () => {
-  return '2025-04-17T10:29:02Z';
+const node_fc8cfd19ad184b46bd25cc65e9e968e7 = () => {
+  return '2025-06-19T17:29:12Z';
 };
