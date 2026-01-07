6ae648b9a44eb001601d0a1629fbc2e19a5aa5ec
nyel-101 <dandevjr@gmail.com>
2026-01-07 10:44:53 +0000

    feat(worker): sanitize view templates

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25a48c2faf544c08a69103ee02a390d4 = () => {
-  return '2026-01-01T08:30:16Z';
+const node_3a022f0986654b17b6922a05b26e59d4 = () => {
+  return '2026-01-07T10:44:53Z';
 };
