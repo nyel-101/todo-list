4ab2dd4d5dc26305ffb9be5d9ee523ef4abde56c
nyel-101 <dandevjr@gmail.com>
2026-02-06 00:42:43 +0000

    fix(core): restructure view templates

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afd2a8d23d27436fa6a7f9096baf003c = () => {
-  return '2026-02-01T02:09:08Z';
+const node_11f95916acb6428a8eab001a6d49d8d3 = () => {
+  return '2026-02-06T00:42:43Z';
 };
