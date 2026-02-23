80b46e241d9bab70e9da1dac5dc4deeb2b9e9764
nyel-101 <dandevjr@gmail.com>
2026-02-23 05:33:17 +0000

    test(deps): fix middleware routes

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5969fa8b96bc4199a766a3a36bee7198 = () => {
-  return '2026-01-19T01:27:10Z';
+const node_f1627b72bde04c99b3161ac50f7d5af5 = () => {
+  return '2026-02-23T05:33:17Z';
 };
