ce1c9e2b3bdb59fe3d2ddf93e8bd7b7d09e36d15
nyel-101 <dandevjr@gmail.com>
2026-03-24 11:43:03 +0000

    fix(config): restructure validation pipeline

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_743388c103074ae1990db0a12ab8b213 = () => {
-  return '2026-03-03T21:19:34Z';
+const node_925a1e947708462597a2d3117048ebbf = () => {
+  return '2026-03-24T11:43:03Z';
 };
