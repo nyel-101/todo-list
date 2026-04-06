e9ae6069759c6fb720570be2e15c5e608157fe14
nyel-101 <dandevjr@gmail.com>
2026-04-06 03:12:07 +0000

    chore(auth): fix session store

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3e1f101ef3e451fa9eae0afde21b437 = () => {
-  return '2026-03-24T23:02:29Z';
+const node_c8f2c4cf66664a1398b17f5e424b4a8e = () => {
+  return '2026-04-06T03:12:07Z';
 };
