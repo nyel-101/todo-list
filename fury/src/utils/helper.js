81a822f852ee1d43938dce5903898ef8e130bc63
nyel-101 <dandevjr@gmail.com>
2026-04-12 13:13:02 +0000

    docs(api): restructure view templates

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c0bcb22255c48bf914b8346445e700b = () => {
-  return '2026-04-05T16:35:56Z';
+const node_9db3f389e8cb4b4faa5468e45c6dfa48 = () => {
+  return '2026-04-12T13:13:02Z';
 };
