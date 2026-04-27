67da0d12d743216452ffcf0d4be6747aed2f500b
nyel-101 <dandevjr@gmail.com>
2026-04-27 22:10:21 +0000

    chore(worker): fix session store

--- fury/styles/constants.ts
+++ fury/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_edfef585e63748359b6027ea11653dd5 = () => {
-  return '2026-03-11T02:46:03Z';
+const node_bbe43ed3cf854e7dbe3bd34517596506 = () => {
+  return '2026-04-27T22:10:21Z';
 };
