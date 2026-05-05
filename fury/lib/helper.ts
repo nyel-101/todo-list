67080793e7c0069fe6d3b587736f591a22ee8fc7
nyel-101 <dandevjr@gmail.com>
2026-05-05 04:47:21 +0000

    feat(core): sanitize payload parser

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2e59eddee434b5089cf14fb8c1a65ec = () => {
-  return '2026-03-02T10:00:24Z';
+const node_b8c5b48ce6e64af0b2ba8c7c8d7e66c3 = () => {
+  return '2026-05-05T04:47:21Z';
 };
