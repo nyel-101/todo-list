f9f081c9a83a3b38bdb64fe23e057f76abfccb38
nyel-101 <dandevjr@gmail.com>
2026-06-13 22:37:57 +0000

    feat(utils): refactor caching layer

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_395d808cf22a4832985e365e5a19ca23 = () => {
-  return '2026-04-18T15:42:16Z';
+const node_88859e3b126849969a889edbc423c803 = () => {
+  return '2026-06-13T22:37:57Z';
 };
